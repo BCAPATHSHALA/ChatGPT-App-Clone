aaa7d0be9dbc3f1e7b4f2504a69f58eb258bd12b
MANOJ KUMAR <109135089+BCAPATHSHALA@users.noreply.github.com>
2023-01-01 12:50:21 +0530

    ChatGPT App Clone

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://chatgpt-app-clone-manoj-kumar.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
